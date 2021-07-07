ac27fdcd45b3abebaaf97d2bc481a7d869418296
Alprod <41442860+Alprod@users.noreply.github.com>
2021-07-07 21:44:47 +0200

    add timer

--- tutoreact/src/index.js
+++ tutoreact/src/index.js
@@ -2,66 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './styles.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
+const docElementRootId = document.getElementById('root');
+const docElementContentId = document.getElementById('content');
+let lastname = 'doe';
+let firstname = 'john';
+const capitalizerFirstLetter = ([first, ...rest], local = navigator.language)=>first.toLocaleUpperCase(local) + rest.join('')
+const user = {
+    nom: lastname,
+    prenom: firstname,
+    anonimyze: "inconnue"
+};
+
+function formatName(props) {
+    if(props.nom && props.prenom){
+        return <h1 className="text-center">Bonjour, {capitalizerFirstLetter(props.nom)} {capitalizerFirstLetter(props.prenom)} !</h1>
     }
+    return props.anonimyze
 }
 
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+function timer(){
+    const element = (
+        <div>
+            {formatName(user)}
+            <h2 className="text-center">Il est <span className="text-danger fs-2 fst-italic fw-bold">{new Date().toLocaleTimeString()}</span></h2>
+        </div>
+    )
+   return ReactDOM.render(element, docElementRootId);
 }
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
+setInterval(timer, 1000)
+
+function Welcome(props) {
+    return <h1  className="text-center mt-5">Bienvenue à toi, {props.name} sur mon test React</h1>
 }
 
-// ========================================
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
+const vue = <Welcome name={user.prenom}/>
+ReactDOM.render(vue,docElementContentId);
